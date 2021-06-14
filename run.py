ff14e89a44654510787d94cb55a3b8e904a9fd62
KieronOC <kieron@lighthouse12.com>
2021-06-14 13:37:49 +0100

    update

--- run.py
+++ run.py
@@ -1,12 +1,33 @@
 import os
-from flask import Flask #Capital F implies it is a class and so is important
+import json
+from flask import Flask, render_template #Capital F implies it is a class and so is important
+
 
 app = Flask(__name__) # Creating an instance of Class Flask 
 
 
 @app.route("/") #decorator
 def index():
-    return "Hello, World"
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    data = []
+    with open ("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html",page_title="About", company = data)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html",page_title="Contact")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html",page_title="Careers")
+
 
 if __name__ == "__main__":
     app.run(
